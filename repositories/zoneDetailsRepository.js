42c0e47332d2f6f80abda71eaed1c81b7d3cfdd7
Mike Galarneau <mbgalarneau@gmail.com>
2019-01-17 13:49:05 -0500

    tweaked the comments to reflect the intent of the SQL for powering off

--- repositories/zoneDetailsRepository.js
+++ repositories/zoneDetailsRepository.js
@@ -25,15 +25,15 @@ let saveZoneUsagePowerOn = (zoneId) => {
 //       zone.id = zoneusage.ZoneId and 
 //       Zoneusage.endDateTime = "2019-01-17 12:15:00";
 
-// -- Option 2
+// -- Option 2 - ANSI SQL
 // UPDATE zoneusage
 // 	INNER JOIN zone ON zoneusage.ZoneId = zone.id
 //  INNER JOIN device ON zone.DeviceId = device.id
-// SET zoneusage.EndDateTime = "2019-01-17 12:11:00"
+// SET zoneusage.EndDateTime = "2019-01-17 12:11:00" (current time)
 // where device.id = XXX and 
 //          device.id = zone.DeviceId and 
 //          zone.id = zoneusage.ZoneId and 
-//          Zoneusage.endDateTime = "1970-01-01 00:00:00";
+//          Zoneusage.endDateTime = "1970-01-01 00:00:00";  (no date)
 
 let saveZoneUsagePowerOff = (deviceId) => {
 
