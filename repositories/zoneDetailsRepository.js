a8fd6c12e820e7caf6c015fe7d11cbb4d3737848
Mike Galarneau <mbgalarneau@gmail.com>
2019-01-17 13:41:35 -0500

    updates zoneUpdate during power on or power off, still working service logic, added moment.js, removed db-model due to formatting problems with git

---
+++ repositories/zoneDetailsRepository.js
@@ -0,0 +1,67 @@
+var moment = require("moment");
+
+var db = require("../models");
+
+// saveZoneUsagePowerOn() - record start time for only the specified zone 
+let saveZoneUsagePowerOn = (zoneId) => {
+
+  let now = moment().format("YYYY-MM-DD HH:mm:ss");
+  let zoneUsage = {
+    startDateTime: now,
+    endDateTime: 0,
+    ZoneId: zoneId
+  };
+
+  return db.ZoneUsage.create(zoneUsage);
+
+};
+
+// saveZoneUsagePowerOff() - record end times for *all active* zones of given device using this SQL logic
+// -- Option 1
+// UPDATE device, zone, zoneusage
+// SET zoneusage.EndDateTime = "2019-01-30 03:03:03"
+// where device.id = 1 and 
+// 	  device.id = zone.DeviceId and 
+//       zone.id = zoneusage.ZoneId and 
+//       Zoneusage.endDateTime = "2019-01-17 12:15:00";
+
+// -- Option 2
+// UPDATE zoneusage
+// 	INNER JOIN zone ON zoneusage.ZoneId = zone.id
+//  INNER JOIN device ON zone.DeviceId = device.id
+// SET zoneusage.EndDateTime = "2019-01-17 12:11:00"
+// where device.id = XXX and 
+//          device.id = zone.DeviceId and 
+//          zone.id = zoneusage.ZoneId and 
+//          Zoneusage.endDateTime = "1970-01-01 00:00:00";
+
+let saveZoneUsagePowerOff = (deviceId) => {
+
+  let now = moment().format("YYYY-MM-DD HH:mm:ss");
+  let noDate = 0;
+
+  // TODO search and update active zone records   
+  return db.ZoneUsage.update({
+    endDateTime: now
+  }, {
+    include: [{
+      model: db.Zone,
+      include: [{
+        model: db.Device,
+        where: {
+          id: deviceId
+        }
+      }],
+      where: {
+        id: db.ZoneUsage.ZoneId
+      }
+    }],
+    where: {
+      endDateTime: noDate
+    }
+  });
+};
+
+
+module.exports.saveZoneUsagePowerOn = saveZoneUsagePowerOn;
+module.exports.saveZoneUsagePowerOff = saveZoneUsagePowerOff;
--- routes/zoneRoutes.js
+++ routes/zoneRoutes.js
@@ -5,30 +5,54 @@ var zoneDetailsRepository = require("../repositories/zoneDetailsRepository");
 module.exports = function (app) {
 
   app.get("/api/device/:deviceId/zones", function (req, res) {
-    db.Zone.findAll({ where: { DeviceId: req.params.deviceId } }).then(function (dbZone) {
+    db.Zone.findAll({
+      where: {
+        DeviceId: req.params.deviceId
+      }
+    }).then(function (dbZone) {
       res.json(dbZone);
-    }).catch(error =>{
+    }).catch(error => {
       console.log(error);
     });
   });
 
   app.get("/api/device/:deviceId/zones/:zoneId/details", function (req, res) {
-    db.ZoneUsage.findAll({ where: { id: req.params.ZoneId } }).then(function (dbZone) {
+    db.ZoneUsage.findAll({
+      where: {
+        id: req.params.ZoneId
+      }
+    }).then(function (dbZone) {
       res.json(dbZone);
     });
   });
 
   app.put("/api/zone/on", function (req, res) {
-    rachioService.turnOnZone(req.body.zoneId).then(resp =>{
+    rachioService.turnOnZone(req.body.zoneId).then(resp => {
       console.log(resp);
-      res.json(resp);
-    });
+    })
+      .then((resp) => {
+        zoneDetailsRepository.saveZoneUsagePowerOn(req.body.zoneId)
+          .then((resp) => {
+            console.log(resp);
+            res.json(resp);
+          }).catch(error => {
+            console.log(error);
+          });
+      });
   });
 
   app.put("/api/zone/off", function (req, res) {
-    rachioService.turnOffZone(req.body.deviceId).then(resp =>{
+    rachioService.turnOffZone(req.body.deviceId).then(resp => {
       console.log(resp);
-      res.json(resp);
-    });
+    })
+      .then((resp) => {
+        zoneDetailsRepository.saveZoneUsagePowerOff(req.body.deviceId)
+          .then(resp => {
+            console.log(resp);
+            res.json(resp);
+          }).catch(error => {
+            console.log(error);
+          });
+      });
   });
 };
