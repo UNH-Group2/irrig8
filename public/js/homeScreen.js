e8bc8c6999167ae1f04af908d7e96a9b4705f86e
Orlando Correa <orlandocorrea12@yahoo.com>
2019-01-19 12:23:06 -0500

    working on navbar

--- public/js/homeScreen.js
+++ public/js/homeScreen.js
@@ -1,13 +1,13 @@
 var password = document.getElementById("password")
-  , confirm_password = document.getElementById("confirm_password");
+  , confirmPassword = document.getElementById("confirm_password");
 
 function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
+  if(password.value !== confirmPassword.value) {
+    confirmPassword.setCustomValidity("Passwords Don't Match");
   } else {
-    confirm_password.setCustomValidity('');
+    confirmPassword.setCustomValidity(" ");
   }
 }
-
 password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+confirmPassword.onkeyup = validatePassword;
+
