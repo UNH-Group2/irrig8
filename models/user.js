9a8073e93b2ec314b45eff2b3ce85c166a439edd
Darren Anastasoff <soleman16@gmail.com>
2019-01-20 23:03:40 -0500

    fixed name of column

--- models/user.js
+++ models/user.js
@@ -47,7 +47,7 @@ module.exports = function (sequelize, DataTypes) {
       }
     },
 
-    rachioTokenExpiration: {
+    rachioTokenExpirationDate: {
       type: DataTypes.DATE,
       allowNull: true,
       validate: {
